3b24d3e2d8cade16767532fcc0742760bc4d5fbf
msarvinen <51877589+msarvinen@users.noreply.github.com>
2023-11-01 22:15:37 +0200

    2023-11-01 22:15

--- check-ping/pinger.sh
+++ check-ping/pinger.sh
@@ -2,9 +2,11 @@
 ping -c 1 -W 1 $1
 if [ $? -gt 0 ]
 then
+    # do your stuff here in case of error
     echo "Error in pinging $1"
     exit 1
 else
+    # do your stuff here in case of connection
     echo "Connection OK"
     exit 0
 fi
